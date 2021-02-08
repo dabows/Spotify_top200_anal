82a8d0af53cd5405b724a59bd25f57487cc60db7
dabows <irazkt@gmail.com>
2021-02-08 12:03:25 -0600

    Trying to animate

--- analysis.py
+++ analysis.py
@@ -6,9 +6,12 @@
 
 
 import pandas as pd
+import numpy as np
 
-import squarify
 import matplotlib.pyplot as plt
+import matplotlib.animation as ani
+
+import bar_chart_race as bar
 
 
 def countArtists():
@@ -40,8 +43,28 @@ def countArtists():
     #print(df_mgd.head(50))
     df_mgd.to_csv('./products/artist_count.csv')
 
+
+def buildPlot(df1,i=int):
+    
+    p = plt.plot(df1.index, df1.values) #note it only returns the dataset, up to the point i
+    
+    
+    
+
 def aniGraph():
-    df = pd.read_csv('./products/artist_count.csv')
+    df = pd.read_csv('./products/artist_streams_timeseries.csv', index_col=0)
+    
+    fig = plt.figure()
+
+    #df.loc['1/1/2017'].sort_values(ascending=False)[:10].iloc[::-1].plot(kind='barh')
+
+    #df['2017-01-01'].sort_values(ascending=0)[:10].plot(kind='bar')
+
+    #animator = ani.FuncAnimation(fig, buildPlot(df), interval = 100)
+    html = bar.bar_chart_race(df, figsize=(4, 2.5), title='COVID-19 Deaths by Country')
+    HTML(html)
+    
+    plt.savefig('test.png', bbox_inches='tight')
 
 if __name__ == '__main__':
     #countArtists()
